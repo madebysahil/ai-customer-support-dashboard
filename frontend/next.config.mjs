9b8b9d754f5f97b25f44378c4c756f8d718e2d3d
madebysahil <sm5807646@gmail.com>
2026-08-07 05:09:08 +0530

    fix: remove output standalone - incompatible with Vercel

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   transpilePackages: ["lucide-react"],
 };
 
 export default nextConfig;
+
